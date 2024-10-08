fd381626bfe7a0b7e883cc797448a95d780bf831
Elpeaze <elpeaze@gmail.com>
2024-10-08 03:37:08 +0000

    Forgot to make the file an executable file!

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -16,7 +16,7 @@ def pascal_triangle(n):
     for idx in range(n):
         row = []
         for val in range(idx + 1):
-            if (val == 0) or (val == i):
+            if (val == 0) or (val == idx):
                 row.append(1)
             else:
                 row.append(triangle[idx - 1][val] + triangle[idx - 1][val - 1])
