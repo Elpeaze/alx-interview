e01e6666027bdc416fd170095a0beeea1898c613
Elpeaze <elpeaze@gmail.com>
2024-10-08 03:34:28 +0000

    Forgot to make the file an executable file!

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+"""This module defines the function `pascal_triangle`"""
 
-"""A function that returns a list of lists of integers 
-     representing the Pascal’s triangle of n
-"""
 
 def pascal_triangle(n):
-    tringle = []
-	for idx in range(n):
+    """
+    Generates Pascal's Triangle up to the nth row.
+
+    Parameters:
+        n (int): The number of rows to generate in Pascal's Triangle.
+
+    Returns:
+        list: A list of lists representing Pascal's Triangle up to the nth row.
+    """
+    triangle = []
+    for idx in range(n):
         row = []
         for val in range(idx + 1):
-            if (val = 0) or (val = idx):
+            if (val == 0) or (val == i):
                 row.append(1)
             else:
                 row.append(triangle[idx - 1][val] + triangle[idx - 1][val - 1])
